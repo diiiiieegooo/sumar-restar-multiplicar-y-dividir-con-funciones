62c587e89ad79a0c2703df59f1c3720e85f9f75d
diiiiieegooo <123724300+diiiiieegooo@users.noreply.github.com>
2023-04-19 12:57:14 +0200

    cambios

---
+++ script.js
@@ -0,0 +1,28 @@
+function sumar(a, b) {
+    return a + b;
+  }
+  
+  function restar(a, b) {
+    return a - b;
+  }
+  
+  function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  function dividir(a, b) {
+    if (b === 0) {
+      console.log("Error: No se puede dividir por cero.");
+      return;
+    }
+    return a / b;
+  }
+  
+  var numero1 = parseInt(prompt("Ingrese el primer número:"));
+  var numero2 = parseInt(prompt("Ingrese el segundo número:"));
+  
+  console.log("Resultado de la suma: " + sumar(numero1, numero2));
+  console.log("Resultado de la resta: " + restar(numero1, numero2));
+  console.log("Resultado de la multiplicación: " + multiplicar(numero1, numero2));
+  console.log("Resultado de la división: " + dividir(numero1, numero2));
+  

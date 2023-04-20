1bb43093b3629069a31e8080790face4f117d737
diiiiieegooo <123724300+diiiiieegooo@users.noreply.github.com>
2023-04-20 10:37:15 +0200

    cambios

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-function sumar(a, b) {
-    return a + b;
-}
+function sumar(num1, num2) {
+    return num1 + num2;
+  }
 
-function restar(a, b) {
-    return a - b;
-}
+  function restar(num1, num2) {
+    return num1 - num2;
+  }
 
-function multiplicar(a, b) {
-    return a * b;
-}
-
-function dividir(a, b) {
-    if (b === 0) {
-        console.log("Error: No se puede dividir por cero.");
-        return;
+  function multiplicar(num1, num2) {
+    return num1 * num2;
+  }
+  
+  function dividir(num1, num2) {
+    if (num2 !== 0) {
+      return num1 / num2;
+    } else {
+      return "No es posible realizar operaciones por cero.";
     }
-    return a / b;
-}
-
-var numero1 = parseInt(prompt("Ingrese el primer número:"));
-var numero2 = parseInt(prompt("Ingrese el segundo número:"));
-
-console.log("Resultado de la suma: " + sumar(numero1, numero2));
-console.log("Resultado de la resta: " + restar(numero1, numero2));
-console.log("Resultado de la multiplicación: " + multiplicar(numero1, numero2));
-console.log("Resultado de la división: " + dividir(numero1, numero2));
+  }
+  
+  alert("¡Bienvenido a mates en linea, a continuación podrá realizar sumas, restas, multiplicaciones o divisiones:");
+  
+  let num1 = parseFloat(prompt("Por favor, ponga el primer número aquí: "));
+  let num2 = parseFloat(prompt("Ahora, ponga el segundo número aquí: "));
+  
+  console.log("El resultado de la suma es: " + sumar(num1, num2));
+  console.log("El resultado de la resta es: " + restar(num1, num2));
+  console.log("El resultado de la multiplicación es: " + multiplicar(num1, num2));
+  console.log("El resultado de la división es: " + dividir(num1, num2));
+  
